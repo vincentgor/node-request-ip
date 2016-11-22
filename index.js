7dc355ed0e8b9dc045029e238a19c917e8672ca5
liangwensen <1149536116@qq.com>
2016-11-22 20:35:08 +0800

    complete some functions

--- index.js
+++ index.js
@@ -4,16 +4,37 @@ module.exports = mw;
 module.exports.getXRealIP = getXRealIP;
 module.exports.getXForwardedFor = getXForwardedFor;
 
+/**
+ * ### get the real client ip address
+ */
 function getXRealIP(req) {
 	var ip = req.headers['x-real-ip'];
-	return ip || 'x-real-ip';
+
+	if (!ip) {
+		ip = req.connection.remoteAddress;
+	}
+
+	return ip;
 }
 
+/**
+ * ### get the proxy ip address list
+ */
 function getXForwardedFor(req) {
-	var ip = req.headers['x-forwarded-for'];
-	return ip || 'x-forwarded-for';
+	var ipArr = req.headers['x-forwarded-for'] || '';
+
+	ipArr.split(/ *, */).filter(Boolean);
+
+	var ip = req.connection.remoteAddress;
+
+	ipArr = ipArr.concat([ip]);
+
+	return ipArr;
 }
 
+/**
+ * ### get the both address above, and return a middleware for express
+ */
 function mw(options) {
 
 	options = options || {};
